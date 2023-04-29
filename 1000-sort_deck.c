59480f8fc834b36bdf5172f4f1a47cac1521fecd
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-04-29 08:20:02 +0200

    Sorting deck

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -8,8 +8,8 @@ void sort_deck(deck_node_t **deck);
 
 /**
  * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
+ * @s1: The first string to be compared...
+ * @s2: The second string to be compared...
  *
  * Return: Positive byte difference if s1 > s2
  *         0 if s1 == s2
